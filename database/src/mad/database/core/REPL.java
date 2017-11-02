198cd6c7ba59b49674846699920c6d2960e29602
Andreas Cederholm <andreas.cederholm@gmail.com>
2017-11-03 00:01:54 +0100

    Remove unused imports.

--- database/src/mad/database/core/REPL.java
+++ database/src/mad/database/core/REPL.java
@@ -3,8 +3,6 @@ package mad.database.core;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
