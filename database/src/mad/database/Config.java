c7672b25895488d528a3f3f039498bbaff798e90
Andreas Cederholm <andreas.cederholm@gmail.com>
2017-11-03 14:12:06 +0100

    Add '.version' and '.help' commands.

---
+++ database/src/mad/database/Config.java
@@ -0,0 +1,8 @@
+package mad.database;
+
+/**
+ *
+ */
+public class Config {
+    public static final String VERSION = "0.1";
+}
--- database/src/mad/database/core/REPL.java
+++ database/src/mad/database/core/REPL.java
@@ -3,15 +3,16 @@ package mad.database.core;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import mad.database.Config;
 
 /**
  *
  */
 public class REPL {
-    
+
     BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    
-    private String readline(){
+
+    private String readline() {
         String line = null;
         try {
             line = in.readLine();
@@ -20,19 +21,40 @@ public class REPL {
         }
         return line;
     }
-    
-    private void run(){
-        while(true){
+
+    private MetaCommandResult runMetaCommand(String query) {
+        if (query.equals(".exit")) {
+            System.exit(0);
+        }
+        if (query.equals(".help")){
+            System.out.println(".exit    Exit this program.");
+            System.out.println(".help    Show available commands.");
+            System.out.println(".version Show version number.");
+            return MetaCommandResult.Success;
+        }
+        if(query.equals(".version")){
+            System.out.printf("MAD version %s\n", Config.VERSION);
+            return MetaCommandResult.Success;
+        }
+        return MetaCommandResult.UnrecognizedCommand;
+    }
+
+    private void run() {
+        System.out.printf("MAD version %s\n", Config.VERSION);
+        while (true) {
             System.out.print("dbname>");
             String query = readline();
-            if(query.equals(".exit")){
-                System.exit(0);
-            }else{
-                System.out.printf("Unrecognized command '%s'\n", query);
+            if(query.charAt(0)=='.'){
+                switch(runMetaCommand(query)){
+                    case Success:
+                        continue;
+                    case UnrecognizedCommand:
+                        System.out.printf("Unrecognized command '%s'\n", query);
+                }
             }
         }
     }
-    
+
     /**
      * @param args the command line arguments
      */
@@ -41,4 +63,9 @@ public class REPL {
         repl.run();
     }
     
+    private enum MetaCommandResult{
+        Success,
+        UnrecognizedCommand
+    }
+
 }
