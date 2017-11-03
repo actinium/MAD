1273cdbfb2af5c18d9566d67f5d22efb401f1e68
Andreas Cederholm <andreas.cederholm@gmail.com>
2017-11-03 19:38:17 +0100

    Change system in to in.

--- database/src/mad/database/core/REPL.java
+++ database/src/mad/database/core/REPL.java
@@ -16,7 +16,7 @@ public class REPL {
     OutputStream out;
 
     public REPL(InputStream in, OutputStream out){
-        this.in = new BufferedReader(new InputStreamReader(System.in));
+        this.in = new BufferedReader(new InputStreamReader(in));
         this.out = out;
     }
     
