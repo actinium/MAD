d7b5883b573a98e9dfbf7353261ab5d4285e858e
Andreas Cederholm <andreas.cederholm@gmail.com>
2017-11-06 01:10:10 +0100

    Add parse method to Parser.

--- database/src/mad/database/sql/Parser.java
+++ database/src/mad/database/sql/Parser.java
@@ -1,8 +1,23 @@
 package mad.database.sql;
 
+import mad.database.sql.Tokenizer.Token;
+
 /**
  *
  */
 public class Parser {
     
+    public Parser(){
+        
+    }
+    
+    private void parse(Token token){
+        // TODO
+    }
+    
+    public void parse(Tokenizer tokenizer){
+        while(tokenizer.hasNext()){
+            parse(tokenizer.next());
+        }
+    }
 }
