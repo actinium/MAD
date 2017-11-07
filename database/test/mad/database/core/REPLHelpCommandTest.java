1cb2c1e589092637bd97a9c1a922a8ba12a7e202
Andreas Cederholm <andreas.cederholm@gmail.com>
2017-11-07 23:04:49 +0100

    Rename test file. Because test files has to end with "Test".

---
+++ database/test/mad/database/core/REPLHelpCommandTest.java
@@ -0,0 +1,32 @@
+package mad.database.core;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ */
+public class REPLHelpCommandTest extends REPLTestBase {
+
+    /**
+     * Test of help command
+     */
+    @Test
+    public void testHelpCommand() {
+
+        writer.println(".help");
+        writer.println(".exit");
+
+        String result = read(reader);
+        String expResult
+                = "dbname>"
+                + ".exit    Exit this program.\n"
+                + ".help    Show available commands.\n"
+                + ".version Show version number.\n"
+                + "dbname>";
+
+        assertEquals(result, expResult);
+
+    }
+
+}
