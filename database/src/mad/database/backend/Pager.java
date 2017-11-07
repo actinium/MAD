13633a5c0857e31b1aeaf24ea48bea0964129ace
Andreas Cederholm <andreas.cederholm@gmail.com>
2017-11-07 22:40:04 +0100

    Add constructor to Pager.

--- database/src/mad/database/backend/Pager.java
+++ database/src/mad/database/backend/Pager.java
@@ -1,8 +1,20 @@
 package mad.database.backend;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
 /**
  *
  */
 public class Pager {
+    
+    private static final int PAGESIZE = 8192;
+    
+    private final RandomAccessFile dbFile;
+    
+    public Pager(File file) throws FileNotFoundException{
+        dbFile = new RandomAccessFile(file, "rwd");
+    }
     
 }
