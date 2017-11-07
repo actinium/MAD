105e820c2a92688b5a375c21f6e8a47fe17a811f
Andreas Cederholm <andreas.cederholm@gmail.com>
2017-11-07 23:47:37 +0100

    Add get and put for DB-Types to Page.

--- database/src/mad/database/backend/Page.java
+++ database/src/mad/database/backend/Page.java
@@ -15,7 +15,36 @@ public class Page {
         this.data = ByteBuffer.wrap(data);
     }
     
-    void read(){
-        
+    public int getInteger(int filePosition){
+        throw new UnsupportedOperationException("Not yet Implemented!");
     }
+    
+    public void putInteger(int filePosition, int number){
+        throw new UnsupportedOperationException("Not yet Implemented!");
+    }
+    
+    public float getFloat(int filePosition){
+        throw new UnsupportedOperationException("Not yet Implemented!");
+    }
+    
+    public void putFloat(int filePosition, float number){
+        throw new UnsupportedOperationException("Not yet Implemented!");
+    }
+    
+    public boolean getBoolean(int filePosition){
+        throw new UnsupportedOperationException("Not yet Implemented!");
+    }
+    
+    public void putBoolean(int filePosition, boolean bool){
+        throw new UnsupportedOperationException("Not yet Implemented!");
+    }
+    
+    public String getString(int fileposition,int length){
+        throw new UnsupportedOperationException("Not yet Implemented!");
+    }
+    
+    public void putString(int fileposition, String string){
+        throw new UnsupportedOperationException("Not yet Implemented!");
+    }
+    
 }
