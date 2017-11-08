8d436b5ec9618963eedcb082885666305358d462
Andreas Cederholm <andreas.cederholm@gmail.com>
2017-11-08 01:30:27 +0100

    Add size variable to PageCache. Without it the page cache can't grow from the start.

--- database/src/mad/database/backend/PageCache.java
+++ database/src/mad/database/backend/PageCache.java
@@ -6,24 +6,46 @@ package mad.database.backend;
 public class PageCache {
 
     private static final int CACHESIZE = 100;
-    
+
     private final Page[] cache = new Page[CACHESIZE];
-    
-    public Page find(int pageStartPosition){
-        for (int i = 0; i < CACHESIZE; i++) {
-            if(cache[i].getPageStartPosition() == pageStartPosition){
-                if(i>0){
-                    Page tmp = cache[i-1];
-                    cache[i-1] = cache[i];
+    private int size = 0;
+
+    /**
+     * Tries finding a page in the cache.
+     *
+     * @param pageStartPosition the start position of the desired page.
+     * @return the desired page if found, otherwise null.
+     */
+    public Page find(int pageStartPosition) {
+        for (int i = 0; i < CACHESIZE && i < size; i++) {
+            if (cache[i].getPageStartPosition() == pageStartPosition) {
+                // Every time we access a Page it gets moved one step forward
+                // in the cache array. This way the most used pages end up
+                // first in the array and the least used is swaped out when
+                // reading in a new page.
+                if (i > 0) {
+                    Page tmp = cache[i - 1];
+                    cache[i - 1] = cache[i];
                     cache[i] = tmp;
                 }
-                return cache[i-1];
+                return cache[i - 1];
             }
         }
         return null;
     }
-    
-    public void put(Page page){
-        cache[CACHESIZE-1] = page;
+
+    /**
+     * Add a new Page to the cache.
+     *
+     * @param page
+     */
+    public void put(Page page) {
+        if (size < CACHESIZE) {
+            cache[size] = page;
+            size++;
+        } else {
+            cache[CACHESIZE - 1] = page;
+        }
     }
+
 }
