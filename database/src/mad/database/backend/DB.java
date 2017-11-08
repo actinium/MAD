2dc06f31d5acf8445dd093acd0ae899129631d0a
Andreas Cederholm <andreas.cederholm@gmail.com>
2017-11-08 03:22:12 +0100

    Add comment about planned functions.

--- database/src/mad/database/backend/DB.java
+++ database/src/mad/database/backend/DB.java
@@ -7,15 +7,25 @@ import java.io.FileNotFoundException;
  *
  */
 public class DB {
-    
+
     private Pager pager;
-    
-    private DB(String filename) throws FileNotFoundException{
+
+    private DB(String filename) throws FileNotFoundException {
         File file = new File(filename);
         pager = new Pager(file);
     }
-    
-    public static DB open(String filename) throws FileNotFoundException{
+
+    public static DB open(String filename) throws FileNotFoundException {
         return new DB(filename);
     }
+
+    // - Create Table
+    // - Alter Table
+    // - Drop Table
+    //
+    // - Insert Row/Rows
+    // - Update Row/Rows
+    // - Delete Row/Rows
+    // 
+    // - Select
 }
