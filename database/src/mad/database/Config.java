f41fdf9f789fdd02257d75d6aed6bb048638ce70
Andreas Cederholm <andreas.cederholm@gmail.com>
2017-11-08 01:26:39 +0100

    Add constant BYTEORDER to Config.

--- database/src/mad/database/Config.java
+++ database/src/mad/database/Config.java
@@ -1,5 +1,7 @@
 package mad.database;
 
+import java.nio.ByteOrder;
+
 /**
  *
  */
@@ -8,5 +10,5 @@ public class Config {
     public static final String MADVERSION = "0.1";
     
     public static final int PAGESIZE = 8192;
-    
+    public static final ByteOrder BYTEORDER = ByteOrder.LITTLE_ENDIAN;
 }
