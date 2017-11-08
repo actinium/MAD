4e9fe3752fdcd6c581e56124ee4130375b17268b
Andreas Cederholm <andreas.cederholm@gmail.com>
2017-11-08 03:08:01 +0100

    Implement Page and PageTest.

--- database/src/mad/database/backend/Page.java
+++ database/src/mad/database/backend/Page.java
@@ -1,54 +1,30 @@
 package mad.database.backend;
 
-import java.nio.ByteBuffer;
-
 /**
  *
  */
 public class Page {
 
-    private final ByteBuffer data;
+    private final byte[] data;
     private final int pageStartPosition;
 
     public Page(int pageStartPosition, byte[] data) {
         this.pageStartPosition = pageStartPosition;
-        this.data = ByteBuffer.wrap(data);
+        this.data = data;
     }
 
     public int getPageStartPosition() {
         return pageStartPosition;
     }
 
-    public int getInteger(int filePosition) {
-        throw new UnsupportedOperationException("Not yet Implemented!");
+    public byte[] getBytes(int filePosition, int length) {
+        byte[] ret = new byte[length];
+        System.arraycopy(data, filePosition - pageStartPosition, ret, 0, length);
+        return ret;
     }
 
-    public void putInteger(int filePosition, int number) {
-        throw new UnsupportedOperationException("Not yet Implemented!");
-    }
-
-    public float getFloat(int filePosition) {
-        throw new UnsupportedOperationException("Not yet Implemented!");
-    }
-
-    public void putFloat(int filePosition, float number) {
-        throw new UnsupportedOperationException("Not yet Implemented!");
-    }
-
-    public boolean getBoolean(int filePosition) {
-        throw new UnsupportedOperationException("Not yet Implemented!");
-    }
-
-    public void putBoolean(int filePosition, boolean bool) {
-        throw new UnsupportedOperationException("Not yet Implemented!");
-    }
-
-    public String getString(int fileposition, int length) {
-        throw new UnsupportedOperationException("Not yet Implemented!");
-    }
-
-    public void putString(int fileposition, String string) {
-        throw new UnsupportedOperationException("Not yet Implemented!");
+    public void putBytes(int filePosition, byte[] bytes, int length) {
+        System.arraycopy(bytes, 0, data, filePosition - pageStartPosition, length);
     }
 
 }
---
+++ database/test/mad/database/backend/PageTest.java
@@ -0,0 +1,71 @@
+package mad.database.backend;
+
+import java.nio.ByteBuffer;
+import static mad.database.Config.BYTEORDER;
+import static mad.database.Config.PAGESIZE;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ */
+public class PageTest {
+
+    public PageTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getPageStartPosition method, of class Page.
+     */
+    @Test
+    public void testPageStart() {
+        Page page0 = new Page(0, new byte[PAGESIZE]);
+        assertEquals(page0.getPageStartPosition(), 0);
+
+        Page page42 = new Page(PAGESIZE * 42, new byte[PAGESIZE]);
+        assertEquals(PAGESIZE * 42, page42.getPageStartPosition());
+    }
+
+    /**
+     * Test of getPageStartPosition method, of class Page.
+     */
+    @Test
+    public void testPageWriteAndRead() {
+        Page page0 = new Page(0, new byte[PAGESIZE]);
+        ByteBuffer bb0w = ByteBuffer.allocate(4).order(BYTEORDER);
+        bb0w.putInt(1234);
+        page0.putBytes(page0.getPageStartPosition() + 100, bb0w.array(), 4);
+        ByteBuffer bb0r = ByteBuffer.wrap(page0.getBytes(page0.getPageStartPosition() + 100, 4)).order(BYTEORDER);
+        int i0 = bb0r.getInt();
+        assertEquals(1234, i0);
+
+        Page page42 = new Page(PAGESIZE * 42, new byte[PAGESIZE]);
+        ByteBuffer bb42w = ByteBuffer.allocate(4).order(BYTEORDER);
+        bb42w.putInt(1100110088);
+        page42.putBytes(page42.getPageStartPosition() + 4242, bb42w.array(), 4);
+        ByteBuffer bb42r = ByteBuffer.wrap(page42.getBytes(page42.getPageStartPosition() + 4242, 4)).order(BYTEORDER);
+        int i42 = bb42r.getInt();
+        assertEquals(1100110088, i42);
+    }
+
+}
