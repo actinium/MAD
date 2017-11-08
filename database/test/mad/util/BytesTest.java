35a0ba05271213b1d59e4b6ace8145a21c75d5ee
Andreas Cederholm <andreas.cederholm@gmail.com>
2017-11-08 16:47:51 +0100

    Add tests for int to byte conversion.

--- database/test/mad/util/BytesTest.java
+++ database/test/mad/util/BytesTest.java
@@ -35,28 +35,99 @@ public class BytesTest {
      * Test of fromInt method, of class Bytes.
      */
     @Test
-    public void testFromInt() {
-        System.out.println("fromInt");
+    public void testFromInt0() {
         int number = 0;
         byte[] expResult = {0, 0, 0, 0};
         byte[] result = Bytes.fromInt(number);
         assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of fromInt method, of class Bytes.
+     */
+    @Test
+    public void testFromInt1234() {
+        int number = 1234;
+        byte[] expResult = {(byte) 0xD2, 0x4, 0, 0};
+        byte[] result = Bytes.fromInt(number);
+        assertArrayEquals(expResult, result);
+    }
+
+    /**
+     * Test of fromInt method, of class Bytes.
+     */
+    @Test
+    public void testFromInt2000000000() {
+        int number = 2000000000;
+        byte[] expResult = {0, (byte) 0x94, (byte) 0x35, (byte) 0x77};
+        byte[] result = Bytes.fromInt(number);
+        assertArrayEquals(expResult, result);
     }
 
     /**
      * Test of toInt method, of class Bytes.
      */
     @Test
-    public void testToInt() {
-        System.out.println("toInt");
-        byte[] bytes = null;
+    public void testToInt0() {
+        byte[] bytes = {0, 0, 0, 0};
         int expResult = 0;
         int result = Bytes.toInt(bytes);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toInt method, of class Bytes.16843009
+     */
+    @Test
+    public void testToInt20171108() {
+        byte[] bytes = {(byte) 0x64, (byte) 0xC9, (byte) 0x33, (byte) 0x01};
+        int expResult = 20171108;
+        int result = Bytes.toInt(bytes);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of toInt method, of class Bytes.
+     */
+    @Test
+    public void testToInt16843009() {
+        byte[] bytes = {(byte) 0x01, (byte) 0x01, (byte) 0x01, (byte) 0x01};
+        int expResult = 16843009;
+        int result = Bytes.toInt(bytes);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of fromInt and toInt methods with 0.
+     */
+    @Test
+    public void testIntZero() {
+        int numberBefore = 0;
+        byte[] result = Bytes.fromInt(numberBefore);
+        int numberAfter = Bytes.toInt(result);
+        assertEquals(numberBefore, numberAfter);
+    }
+
+    /**
+     * Test of fromInt and toInt methods with Integer.MAX_VALUE.
+     */
+    @Test
+    public void testIntMax() {
+        int numberBefore = Integer.MAX_VALUE;
+        byte[] result = Bytes.fromInt(numberBefore);
+        int numberAfter = Bytes.toInt(result);
+        assertEquals(numberBefore, numberAfter);
+    }
+
+    /**
+     * Test of fromInt and toInt methods with Integer.MIN_VALUE.
+     */
+    @Test
+    public void testIntMin() {
+        int numberBefore = Integer.MIN_VALUE;
+        byte[] result = Bytes.fromInt(numberBefore);
+        int numberAfter = Bytes.toInt(result);
+        assertEquals(numberBefore, numberAfter);
     }
 
     /**
@@ -64,7 +135,6 @@ public class BytesTest {
      */
     @Test
     public void testFromFloat() {
-        System.out.println("fromFloat");
         float number = 0.0F;
         byte[] expResult = null;
         byte[] result = Bytes.fromFloat(number);
@@ -78,7 +148,6 @@ public class BytesTest {
      */
     @Test
     public void testToFloat() {
-        System.out.println("toFloat");
         byte[] bytes = null;
         float expResult = 0.0F;
         float result = Bytes.toFloat(bytes);
@@ -92,7 +161,6 @@ public class BytesTest {
      */
     @Test
     public void testFromBoolean() {
-        System.out.println("fromBoolean");
         boolean bool = false;
         byte[] expResult = null;
         byte[] result = Bytes.fromBoolean(bool);
@@ -106,7 +174,6 @@ public class BytesTest {
      */
     @Test
     public void testToBoolean() {
-        System.out.println("toBoolean");
         byte[] bytes = null;
         boolean expResult = false;
         boolean result = Bytes.toBoolean(bytes);
@@ -120,7 +187,6 @@ public class BytesTest {
      */
     @Test
     public void testFromString() {
-        System.out.println("fromString");
         String string = "";
         byte[] expResult = null;
         byte[] result = Bytes.fromString(string);
@@ -134,7 +200,6 @@ public class BytesTest {
      */
     @Test
     public void testToString() {
-        System.out.println("toString");
         byte[] bytes = null;
         boolean expResult = false;
         boolean result = Bytes.toString(bytes);
