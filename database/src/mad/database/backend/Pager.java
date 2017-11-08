640a92416b70ba90eaa4fd66ba23ae50a1402fb8
Andreas Cederholm <andreas.cederholm@gmail.com>
2017-11-08 02:11:51 +0100

    Add size parameter to writeBytes in Pager.

--- database/src/mad/database/backend/Pager.java
+++ database/src/mad/database/backend/Pager.java
@@ -21,11 +21,23 @@ public class Pager {
         currentPage = null;
     }
 
-    public Page newPage() {
+    /**
+     * Get a new Page.
+     * Returns a page from the free-list or, if the free-list is empty,
+     * allocates a new page.
+     * 
+     * @return the startPosition of the page
+     */
+    public int newPage() {
         throw new UnsupportedOperationException("Not yet Implemented!");
     }
 
-    public void freePage() {
+    /**
+     * Frees a page when it's no longer needed.
+     * 
+     * @param starPosition the startPosition of the page.
+     */
+    public void freePage(int starPosition) {
         throw new UnsupportedOperationException("Not yet Implemented!");
     }
 
@@ -34,7 +46,7 @@ public class Pager {
     }
 
     public void writeInteger(int filePosition, int number) {
-        writeBytes(filePosition, ByteBuffer.allocate(4).order(BYTEORDER).putInt(number));
+        writeBytes(filePosition, ByteBuffer.allocate(4).order(BYTEORDER).putInt(number),4);
     }
 
     public float readFloat(int filePosition) {
@@ -42,7 +54,7 @@ public class Pager {
     }
 
     public void writeFloat(int filePosition, float number) {
-        writeBytes(filePosition, ByteBuffer.allocate(4).order(BYTEORDER).putFloat(number));
+        writeBytes(filePosition, ByteBuffer.allocate(4).order(BYTEORDER).putFloat(number),4);
     }
 
     public boolean readBoolean(int filePosition) {
@@ -51,7 +63,7 @@ public class Pager {
     }
 
     public void writeBoolean(int filePosition, boolean bool) {
-        writeBytes(filePosition, ByteBuffer.allocate(1).order(BYTEORDER).put((byte) (bool ? 1 : 0)));
+        writeBytes(filePosition, ByteBuffer.allocate(1).order(BYTEORDER).put((byte) (bool ? 1 : 0)),1);
     }
 
     public String readString(int fileposition, int length) {
@@ -69,7 +81,7 @@ public class Pager {
         throw new UnsupportedOperationException("Not yet Implemented!");
     }
 
-    private void writeBytes(int filePosition, ByteBuffer bytes) {
+    private void writeBytes(int filePosition, ByteBuffer bytes, int size) {
         // - is 0 <= filePosition < file-size?
         // - is page == currentPage?
         // - is page in PageCache?
