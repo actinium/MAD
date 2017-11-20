6e9dc16aa094b6f32abcb64ae9b39f08572f995b
Andreas Cederholm <andreas.cederholm@gmail.com>
2017-11-20 16:57:59 +0100

    Formating.

--- database/test/mad/util/NullBitMapTest.java
+++ database/test/mad/util/NullBitMapTest.java
@@ -41,7 +41,7 @@ public class NullBitMapTest {
             assertEquals(false, instance.isNull(i));
         }
     }
-    
+
     /**
      *
      */
@@ -56,14 +56,14 @@ public class NullBitMapTest {
         instance.setNotNull(8);
         instance.setNotNull(13);
         for (int i = 0; i < 64; i++) {
-            if(i==5 || i==12 || i==14){
-                assertTrue( instance.isNull(i));
-            }else{
-                assertFalse( instance.isNull(i));
+            if (i == 5 || i == 12 || i == 14) {
+                assertTrue(instance.isNull(i));
+            } else {
+                assertFalse(instance.isNull(i));
             }
         }
     }
-    
+
     /**
      *
      */
@@ -79,14 +79,14 @@ public class NullBitMapTest {
         instance.setNotNull(13);
         instance = Bytes.toNullBitMap(Bytes.fromNullBitMap(instance));
         for (int i = 0; i < 64; i++) {
-            if(i==5 || i==12 || i==14){
-                assertTrue( instance.isNull(i));
-            }else{
-                assertFalse( instance.isNull(i));
+            if (i == 5 || i == 12 || i == 14) {
+                assertTrue(instance.isNull(i));
+            } else {
+                assertFalse(instance.isNull(i));
             }
         }
     }
-    
+
     /**
      *
      */
@@ -97,14 +97,14 @@ public class NullBitMapTest {
         instance.setNull(31);
         instance.setNull(63);
         for (int i = 0; i < 64; i++) {
-            if(i==0 || i==31 || i==63){
-                assertTrue( instance.isNull(i));
-            }else{
-                assertFalse( instance.isNull(i));
+            if (i == 0 || i == 31 || i == 63) {
+                assertTrue(instance.isNull(i));
+            } else {
+                assertFalse(instance.isNull(i));
             }
         }
     }
-    
+
     /**
      *
      */
@@ -116,10 +116,10 @@ public class NullBitMapTest {
         instance.setNull(63);
         instance = Bytes.toNullBitMap(Bytes.fromNullBitMap(instance));
         for (int i = 0; i < 64; i++) {
-            if(i==0 || i==31 || i==63){
-                assertTrue( instance.isNull(i));
-            }else{
-                assertFalse( instance.isNull(i));
+            if (i == 0 || i == 31 || i == 63) {
+                assertTrue(instance.isNull(i));
+            } else {
+                assertFalse(instance.isNull(i));
             }
         }
     }
@@ -133,7 +133,7 @@ public class NullBitMapTest {
         byte[] bytes = Bytes.fromNullBitMap(instance);
         assertEquals(8, bytes.length);
     }
-    
+
     /**
      *
      */
@@ -146,34 +146,34 @@ public class NullBitMapTest {
         instance.setNull(29);
         instance.setNull(55);
         byte[] bytes = Bytes.fromNullBitMap(instance);
-        byte[] expBytes = {0b100000,(byte)0b10000000,0b0,0b110000,0b0,0b0,(byte)0b10000000,0b0};
+        byte[] expBytes = {0b100000, (byte) 0b10000000, 0b0, 0b110000, 0b0, 0b0, (byte) 0b10000000, 0b0};
         assertEquals(expBytes.length, bytes.length);
         assertArrayEquals(expBytes, bytes);
     }
 
     /**
-     * 
+     *
      */
     @Test
     public void testFromBytes() {
-        byte[] bytes = {0,0,0,0,0,0,0,0};
+        byte[] bytes = {0, 0, 0, 0, 0, 0, 0, 0};
         NullBitMap nullMap = Bytes.toNullBitMap(bytes);
         for (int i = 0; i < 64; i++) {
             assertFalse(nullMap.isNull(i));
         }
     }
-    
+
     /**
-     * 
+     *
      */
     @Test
     public void testFromBytes2() {
-        byte[] bytes = {0b100000,(byte)0b10000000,0b0,0b110000,0b0,0b0,(byte)0b10000000,0b0};
+        byte[] bytes = {0b100000, (byte) 0b10000000, 0b0, 0b110000, 0b0, 0b0, (byte) 0b10000000, 0b0};
         NullBitMap nullMap = Bytes.toNullBitMap(bytes);
         for (int i = 0; i < 64; i++) {
-            if(i== 5 || i==15||i==28||i==29||i==55){
+            if (i == 5 || i == 15 || i == 28 || i == 29 || i == 55) {
                 assertTrue(nullMap.isNull(i));
-            }else{
+            } else {
                 assertFalse(nullMap.isNull(i));
             }
         }
