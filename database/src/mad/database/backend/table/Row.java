8afb27c6c316ca6a7e304b0aaeee0db81f02fc09
Andreas Cederholm <andreas.cederholm@gmail.com>
2017-11-23 00:05:05 +0100

    Edit throws comments and format lines.

--- database/src/mad/database/backend/table/Row.java
+++ database/src/mad/database/backend/table/Row.java
@@ -23,117 +23,129 @@ public interface Row {
      *
      * @param columnNumber
      * @return
-     * @throws mad.database.backend.table.Row.NoSuchColumnException
-     * @throws mad.database.backend.table.Row.TypeMismatchException
+     * @throws Row.NoSuchColumnException
+     * @throws Row.TypeMismatchException
      * @throws IOException
      */
-    public int getInteger(int columnNumber) throws NoSuchColumnException, TypeMismatchException, IOException;
+    public int getInteger(int columnNumber) throws NoSuchColumnException,
+            TypeMismatchException, IOException;
 
     /**
      *
      * @param columnName
      * @return
-     * @throws mad.database.backend.table.Row.NoSuchColumnException
-     * @throws mad.database.backend.table.Row.TypeMismatchException
+     * @throws Row.NoSuchColumnException
+     * @throws Row.TypeMismatchException
      * @throws IOException
      */
-    public int getInteger(String columnName) throws NoSuchColumnException, TypeMismatchException, IOException;
+    public int getInteger(String columnName) throws NoSuchColumnException,
+            TypeMismatchException, IOException;
 
     /**
      *
      * @param columnNumber
      * @return
-     * @throws mad.database.backend.table.Row.NoSuchColumnException
-     * @throws mad.database.backend.table.Row.TypeMismatchException
+     * @throws Row.NoSuchColumnException
+     * @throws Row.TypeMismatchException
      * @throws IOException
      */
-    public float getFloat(int columnNumber) throws NoSuchColumnException, TypeMismatchException, IOException;
+    public float getFloat(int columnNumber) throws NoSuchColumnException,
+            TypeMismatchException, IOException;
 
     /**
      *
      * @param columnName
      * @return
-     * @throws mad.database.backend.table.Row.NoSuchColumnException
-     * @throws mad.database.backend.table.Row.TypeMismatchException
+     * @throws Row.NoSuchColumnException
+     * @throws Row.TypeMismatchException
      * @throws IOException
      */
-    public float getFloat(String columnName) throws NoSuchColumnException, TypeMismatchException, IOException;
+    public float getFloat(String columnName) throws NoSuchColumnException,
+            TypeMismatchException, IOException;
 
     /**
      *
      * @param columnNumber
      * @return
-     * @throws mad.database.backend.table.Row.NoSuchColumnException
-     * @throws mad.database.backend.table.Row.TypeMismatchException
+     * @throws Row.NoSuchColumnException
+     * @throws Row.TypeMismatchException
      * @throws IOException
      */
-    public boolean getBoolean(int columnNumber) throws NoSuchColumnException, TypeMismatchException, IOException;
+    public boolean getBoolean(int columnNumber) throws NoSuchColumnException,
+            TypeMismatchException, IOException;
 
     /**
      *
      * @param columnName
      * @return
-     * @throws mad.database.backend.table.Row.NoSuchColumnException
-     * @throws mad.database.backend.table.Row.TypeMismatchException
+     * @throws Row.NoSuchColumnException
+     * @throws Row.TypeMismatchException
      * @throws IOException
      */
-    public boolean getBoolean(String columnName) throws NoSuchColumnException, TypeMismatchException, IOException;
+    public boolean getBoolean(String columnName) throws NoSuchColumnException,
+            TypeMismatchException, IOException;
 
     /**
      *
      * @param columnNumber
      * @return
-     * @throws mad.database.backend.table.Row.NoSuchColumnException
-     * @throws mad.database.backend.table.Row.TypeMismatchException
+     * @throws Row.NoSuchColumnException
+     * @throws Row.TypeMismatchException
      * @throws IOException
      */
-    public String getString(int columnNumber) throws NoSuchColumnException, TypeMismatchException, IOException;
+    public String getString(int columnNumber) throws NoSuchColumnException,
+            TypeMismatchException, IOException;
 
     /**
      *
      * @param columnName
      * @return
-     * @throws mad.database.backend.table.Row.NoSuchColumnException
-     * @throws mad.database.backend.table.Row.TypeMismatchException
+     * @throws Row.NoSuchColumnException
+     * @throws Row.TypeMismatchException
      * @throws IOException
      */
-    public String getString(String columnName) throws NoSuchColumnException, TypeMismatchException, IOException;
+    public String getString(String columnName) throws NoSuchColumnException,
+            TypeMismatchException, IOException;
 
     /**
      *
      * @param columnNumber
      * @return
-     * @throws mad.database.backend.table.Row.NoSuchColumnException
+     * @throws Row.NoSuchColumnException
      * @throws IOException
      */
-    public boolean isNull(int columnNumber) throws NoSuchColumnException, IOException;
+    public boolean isNull(int columnNumber) throws NoSuchColumnException,
+            IOException;
 
     /**
      *
      * @param columnName
      * @return
-     * @throws mad.database.backend.table.Row.NoSuchColumnException
+     * @throws Row.NoSuchColumnException
      * @throws IOException
      */
-    public boolean isNull(String columnName) throws NoSuchColumnException, IOException;
+    public boolean isNull(String columnName) throws NoSuchColumnException,
+            IOException;
 
     /**
      *
      * @param columnNumber
      * @return
-     * @throws mad.database.backend.table.Row.NoSuchColumnException
+     * @throws Row.NoSuchColumnException
      * @throws IOException
      */
-    public String getName(int columnNumber) throws NoSuchColumnException, IOException;
+    public String getName(int columnNumber) throws NoSuchColumnException,
+            IOException;
 
     /**
      *
      * @param columnNumber
      * @return
-     * @throws mad.database.backend.table.Row.NoSuchColumnException
+     * @throws Row.NoSuchColumnException
      * @throws IOException
      */
-    public Schema.Field.Type getType(int columnNumber) throws NoSuchColumnException, IOException;
+    public Schema.Field.Type getType(int columnNumber) throws
+            NoSuchColumnException, IOException;
 
     /**
      *
