60e8c4a70e0027f4a38716c307b815710b0d4c2e
Andreas Cederholm <andreas.cederholm@gmail.com>
2017-11-24 16:48:07 +0100

    Formating.

--- database/src/mad/database/backend/table/WritableRow.java
+++ database/src/mad/database/backend/table/WritableRow.java
@@ -12,130 +12,126 @@ public interface WritableRow extends Row {
      * @return
      */
     @Override
-    public WritableRow next();
-    
+    WritableRow next();
+
     /**
-     * 
+     *
      * @param columnNumber
      * @param value
      * @throws Row.NoSuchColumnException
      * @throws Row.TypeMismatchException
-     * @throws IOException 
+     * @throws IOException
      */
-    public void setInteger(int columnNumber, int value) throws
-            NoSuchColumnException, TypeMismatchException, IOException;
+    void setInteger(int columnNumber, int value) throws NoSuchColumnException,
+            TypeMismatchException, IOException;
 
     /**
-     * 
+     *
      * @param columnName
      * @param value
      * @throws Row.NoSuchColumnException
      * @throws Row.TypeMismatchException
-     * @throws IOException 
+     * @throws IOException
      */
-    public void setInteger(String columnName, int value) throws
-            NoSuchColumnException, TypeMismatchException, IOException;
+    void setInteger(String columnName, int value) throws NoSuchColumnException,
+            TypeMismatchException, IOException;
 
     /**
-     * 
+     *
      * @param columnNumber
      * @param value
      * @throws Row.NoSuchColumnException
      * @throws Row.TypeMismatchException
-     * @throws IOException 
+     * @throws IOException
      */
-    public void setFloat(int columnNumber, float value) throws
-            NoSuchColumnException, TypeMismatchException, IOException;
+    void setFloat(int columnNumber, float value) throws NoSuchColumnException,
+            TypeMismatchException, IOException;
 
     /**
-     * 
+     *
      * @param columnName
      * @param value
      * @throws Row.NoSuchColumnException
      * @throws Row.TypeMismatchException
-     * @throws IOException 
+     * @throws IOException
      */
-    public void setFloat(String columnName, float value) throws
+    void setFloat(String columnName, float value) throws
             NoSuchColumnException, TypeMismatchException, IOException;
 
     /**
-     * 
+     *
      * @param columnNumber
      * @param value
      * @throws Row.NoSuchColumnException
      * @throws Row.TypeMismatchException
-     * @throws IOException 
+     * @throws IOException
      */
-    public void setBoolean(int columnNumber, boolean value) throws
-            NoSuchColumnException, TypeMismatchException, IOException;
+    void setBoolean(int columnNumber, boolean value) throws NoSuchColumnException,
+            TypeMismatchException, IOException;
 
     /**
-     * 
+     *
      * @param columnName
      * @param value
      * @throws Row.NoSuchColumnException
      * @throws Row.TypeMismatchException
-     * @throws IOException 
+     * @throws IOException
      */
-    public void setBoolean(String columnName, boolean value) throws
+    void setBoolean(String columnName, boolean value) throws
             NoSuchColumnException, TypeMismatchException, IOException;
 
     /**
-     * 
+     *
      * @param columnNumber
      * @param value
      * @throws Row.NoSuchColumnException
      * @throws Row.TypeMismatchException
-     * @throws IOException 
+     * @throws IOException
      */
-    public void setString(int columnNumber, String value) throws
-            NoSuchColumnException, TypeMismatchException, IOException;
+    void setString(int columnNumber, String value) throws NoSuchColumnException,
+            TypeMismatchException, IOException;
 
     /**
-     * 
+     *
      * @param columnName
      * @param Value
      * @throws Row.NoSuchColumnException
      * @throws Row.TypeMismatchException
-     * @throws IOException 
+     * @throws IOException
      */
-    public void setString(String columnName, String Value) throws
-            NoSuchColumnException, TypeMismatchException, IOException;
+    void setString(String columnName, String Value) throws NoSuchColumnException,
+            TypeMismatchException, IOException;
 
     /**
-     * 
+     *
      * @param columnNumber
      * @throws Row.NoSuchColumnException
-     * @throws IOException 
+     * @throws IOException
      */
-    public void setNull(int columnNumber) throws NoSuchColumnException,
-            IOException;
+    void setNull(int columnNumber) throws NoSuchColumnException, IOException;
 
     /**
-     * 
+     *
      * @param columnName
      * @throws Row.NoSuchColumnException
-     * @throws IOException 
+     * @throws IOException
      */
-    public void setNull(String columnName) throws NoSuchColumnException,
-            IOException;
+    void setNull(String columnName) throws NoSuchColumnException, IOException;
 
     /**
-     * 
+     *
      * @param columnNumber
      * @throws Row.NoSuchColumnException
-     * @throws IOException 
+     * @throws IOException
      */
-    public void setNotNull(int columnNumber) throws NoSuchColumnException,
-            IOException;
+    void setNotNull(int columnNumber) throws NoSuchColumnException, IOException;
 
     /**
-     * 
+     *
      * @param columnName
      * @throws Row.NoSuchColumnException
-     * @throws IOException 
+     * @throws IOException
      */
-    public void setNotNull(String columnName) throws NoSuchColumnException,
-            IOException;
+    void setNotNull(String columnName) throws NoSuchColumnException, IOException;
 
 }
