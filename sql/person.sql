9017bf81f3e2bb7c8aca63ce5e745cc47fac94e9
Andreas Cederholm <andreas.cederholm@gmail.com>
2018-04-24 00:20:10 +0200

    Add example sql file.

---
+++ sql/person.sql
@@ -0,0 +1,14 @@
+create table person(
+	id   int,
+	name varchar(42),
+	age  int
+);
+
+insert into person values(1,'Alice',23);
+insert into person values(2,'Bob',40);
+insert into person values(3,'Charlie',12);
+insert into person values(4,'Hannah',29);
+insert into person values(5,'Sara',17);
+insert into person values(6,'Tom',16);
+insert into person values(7,'Gustav',32);
+
